f4d4b4591a55492cd8533e37426e961e2fd6a2cd
Bernardo Bernardi <bernardobernardi08@gmail.com>
2025-08-24 17:22:34 -0300

    Final

--- Movimentação.cpp
+++ Movimentação.cpp
@@ -1,51 +1,21 @@
-#include <Servo.h>  // Incluindo biblioteca Servo
-
-
-Servo servo;  // Criando Objeto Servo, para controle do Servo Motor
-
-uint32_t TempLuzFreio, TempPiscaAlerta, TempSetaDireita, TempSetaEsquerda, bip_tensao, temp;  // Variáveis necessárias para contagem de tempo em milis segundos (substitui a função delay) - (variáveis uint32_t são inteiros de 32 bits sem sinal)
-
 int JoyAX = 255, JoyAY = 255;  // Variáveis Globais para receberem os valores dos eixo X e Y do JoyStick A
 int JoyBX = 90, JoyBY = 90;    // Variáveis Globais para receberem os valores dos eixo X e Y do JoyStick B
 // As variáveis JoyAX e JoyBY não serão utilizadas neste código
 
-// Variáveis Globais para auxiliar na identificação de quando um botão foi pressionado no celular
-boolean Farol_Ativo = 0;
-boolean Pisca_Ativo = 0;
-boolean SetaD_Ativo = 0;
-boolean SetaE_Ativo = 0;
-boolean LuzRe_Ativo = 0;
-boolean LuzFreio_Ativo = 0;
-
-float tensao = 0;
-
-int Velocidade = 2;
 
 // Variável Global para armazenar os caracteres que o arduino receber do celular via módulo bluetooth
 char DadosRecebidos;
-
-#define IN1 5
-#define IN2 6
-#define IN3 3
-#define IN4 4
-#define ENA 9
-
+char F = 'f';
+char R = 'r';
 
 void setup() {
 
   Serial.begin(9600);  // Inicializando comunicação Serial Padrão nas portas 0 e 1 do arduino
-  Serial.println("PROJETO FUSCA BLUETOOTH RC");
-  servo.attach(7);  // Informando que o servo motor está conectado ao pino digital 7 do Arduino
+  Serial.println("TransitApp");  
 
-  pinMode(3, OUTPUT);   // Leds - Farol
-  pinMode(4, OUTPUT);   // Buzzer - Buzinha
-  pinMode(5, OUTPUT);   // Ponte H
-  pinMode(6, OUTPUT);   // Ponte H
-  pinMode(7, OUTPUT);   // Led - Seta Esquerda Dianteira
   pinMode(8, OUTPUT);   // Led - Seta Direita Dianteira
   pinMode(9, OUTPUT);   // Led - Lanterna / Luz de Freio / Luz de Ré / Seta Direita Trazeira
-  pinMode(10, OUTPUT);  // Led - Lanterna / Luz de Freio / Luz de Ré / Seta Esquerda Trazeira
-  pinMode(11, INPUT);
+ 
 
 
  
@@ -55,26 +25,13 @@ void setup() {
   Serial.println("clear_panel()");
   Serial.println("set_grid_size(10,5)");
   Serial.println("add_text(4,2,large,C,TransitApp,245,240,245)");
+  Serial.println("add_switch(7,4,1,P,p,0,0)");
+  Serial.println("add_switch(0,4,1,F,f,0,0)");
   Serial.println("add_free_pad(7,1,10,170,0,50,C,D)");
   Serial.println("add_free_pad(0,1,0,510,0,50,A,B)");
   Serial.println("run()");
   Serial.println("*");
  
-
-
-
-
-  // Emite um bip ao ligar o Jeep
-  tone(4, 1000, 100);
-  delay(100);
-  tone(4, 800, 100);
-  delay(100);
-  tone(4, 600, 100);
-  delay(100);
-  tone(4, 400, 100);
-  delay(100);
-  tone(4, 200, 100);
-  delay(100);
 }
 
 
@@ -85,7 +42,9 @@ void loop() {
 
 
 DadosRecebidos = Serial.read();
-  
+Serial.println(F);
+Serial.println(R);
+Serial.println(DadosRecebidos);
   if (DadosRecebidos > 0) {}
   if (DadosRecebidos == 'A') {
     JoyAX = Serial.parseInt();
@@ -110,72 +69,39 @@ DadosRecebidos = Serial.read();
     }
   }
 
-  servo.write(JoyBX);
+  if (JoyAY <= 250 || F == 'F') {  // FRENTE - quando o bastão do joystick virtual é empurado para cima (posição central é = 255, todo para cima = 0)
 
-  int aux = 0;  // variável para auxiliar na converasão dos valores do joystick para o padrão PWM de 0 a 255
-
-  if (JoyAY <= 250) {  // FRENTE - quando o bastão do joystick virtual é empurado para cima (posição central é = 255, todo para cima = 0)
-
-    if (Velocidade == 1) {
-      aux = map(JoyAY, 260, 510, 20, 190);  // Velocidade 01 - LENTA
-    } else {
-      aux = map(JoyAY, 260, 510, 20, 255); // Velocidade 02 - RÁPIDA
-    }
     // Controle da Ponte H
-    digitalWrite(8, LOW);            // ativa pino ponte H com valor LOW para que o veículo se movimente para frente
-    digitalWrite(9, HIGH);            // ativa pino ponte H com o valor da variável Pot (controle de aceleração)
-    digitalWrite(12, LOW);            // ativa pino ponte H com valor LOW para que o veículo se movimente para frente
-    digitalWrite(11, HIGH);         // variável TempSetaDireita recebe o tempo atual
-                                    // Luz de Ré
-  
-
+    digitalWrite(8, HIGH);            // ativa pino ponte H com valor LOW para que o veículo se movimente para frente
+    digitalWrite(9, LOW);            // ativa pino ponte H com o valor da variável Pot (controle de aceleração)       
   } 
 
-  else if (JoyAY >= 260) {  // RÉ - quando o bastão do joystick virtual é empurado para baixo (posição central é = 255, todo para baixo = 510)
-    if (Velocidade == 1) {
-      aux = map(JoyAY, 260, 510, 20, 190);
-    } else {
-      aux= map(JoyAY, 260, 510, 20, 255);
-    }
+  else if (JoyAY >= 260 || R == 'R') {  // RÉ - quando o bastão do joystick virtual é empurado para baixo (posição central é = 255, todo para baixo = 510)
     // Controle da Ponte H
   
-    digitalWrite(8, HIGH);  // ativa pino ponte H com o valor da variável Pot (controle de aceleração)
-    digitalWrite(9, LOW);  // ativa pino ponte H com valor LOW para que o veículo se movimente para trás
-    digitalWrite(12, HIGH);  // ativa pino ponte H com o valor da variável Pot (controle de aceleração)
-    digitalWrite(11, LOW);  // ativa pino ponte H com valor LOW para que o veículo se movimente para trás
-
-    //Serial.print(" Re PWM: "); // Mostra no Monitor Serial
-    //Serial.println(aux); // Mostra no Monitor Serial os valores de aux
-
-    // Luz de Ré
+    digitalWrite(8, LOW);  // ativa pino ponte H com o valor da variável Pot (controle de aceleração)
+    digitalWrite(9, HIGH);  // ativa pino ponte H com valor LOW para que o veículo se movimente para trás
     
-
-    TempLuzFreio = millis();  // variável TempSetaDireita recebe o tempo atual
   }
 
   else {                  // MOTOR PARADO
     
     analogWrite(8, LOW);  // desativa as duas portas da ponte h
     analogWrite(9, LOW);  // desativa as duas portas da ponte h
-    analogWrite(12, LOW);  // desativa as duas portas da ponte h
-    analogWrite(11, LOW);  // desativa as duas portas da ponte h
 
-    // Luz de Ré
-  
-    // GERENCIA CONTROLE DA LUZ DE FREIO, SETAS, LENTERNA E PISCA ALERTA DO FUSCA
-    // if ((Pisca_Ativo == LOW) && (SetaD_Ativo == LOW) && (SetaE_Ativo == LOW)) {
-    // Verifica se as variáveis indicando atividade do pisca, seta direita e seta esquerda estão todas desativadas.
-    if (millis() - TempLuzFreio < 50) {
-      // Se o tempo decorrido desde a última atualização da luz de freio for menor que 150 milissegundos:
-      // digitalWrite(9, LOW);
-      //  Serial.println(" luz freio apagado - 150");
-      // Apaga os LEDs conectados aos pinos 9 e 10 e imprime uma mensagem indicando a condição.
-    } else if ((millis() - TempLuzFreio > 50) && (millis() - TempLuzFreio < 650)) {
-      // Se o tempo decorrido estiver entre 300 e 1000 milissegundos:
-      
-     
-    // }
-    }
+  }
+
+  if (DadosRecebidos == 'P') {
+    F = 'F';
+  }
+  else if (DadosRecebidos =='p') {
+    F ='f';
+  }
+  if (DadosRecebidos == 'F') {
+    R = 'R';
+  }
+  else if (DadosRecebidos == 'f') {
+    R = 'r';
   }
    
 }
